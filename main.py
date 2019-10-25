3ea52c168b7e161e7c5ef8f1d663cb1c29478bcc
niddumulu <36333121+niddumulu@users.noreply.github.com>
2019-10-25 21:03:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ import requests
 import re
 
 def get_url():
-    contents = requests.get('https://random.dog/woof.json').json()    
+    contents = requests.get('https://random.dog/woof.json').json()
     url = contents['url']
     return url
 
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
 def bop(bot, update):
-    url = get_url()
+    url = get_image_url()
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
 
 def main():
-    
     updater = Updater('875680961:AAGf315uqIwyEankI0lFTN_9H-_ibb3O1nA', use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('bop',bop))
